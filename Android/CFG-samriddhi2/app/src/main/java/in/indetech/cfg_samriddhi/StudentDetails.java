e3ac4fe6bc2e95ac7bd247d0be2053996d04248a
darshan <darshan.gowda008@gmail.com>
2016-07-09 20:44:53 +0530

    added student details

--- Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetails.java
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetails.java
@@ -1,7 +1,8 @@
 package in.indetech.cfg_samriddhi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class StudentDetails extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class StudentDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
+
+
+    }
+
+    public void next(View view) {
+
+
     }
 }
