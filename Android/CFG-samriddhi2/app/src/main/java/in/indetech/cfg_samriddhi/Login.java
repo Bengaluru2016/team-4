df4e2ea0996ac5df9bd93d571e9ead1afe3c2bb3
darshan <darshan.gowda008@gmail.com>
2016-07-09 18:13:55 +0530

    Authentication for surveyer

--- Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/Login.java
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/Login.java
@@ -1,13 +1,113 @@
 package in.indetech.cfg_samriddhi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.logging.LogManager;
 
 public class Login extends AppCompatActivity {
+
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init() {
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+    }
+
+    public void login_method(View view) {
+
+        String user_name = username.getText().toString();
+        String pass_word = password.getText().toString();
+
+        authenticate(user_name, pass_word);
+
+    }
+
+    private void authenticate(final String user_name, final String pass_word) {
+
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                String Response = "";
+
+                URL url;
+                try {
+                    url = new URL(Constants.URL + "login.php");
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setRequestMethod("POST");
+
+                    Uri.Builder builder = new Uri.Builder()
+                            .appendQueryParameter("username", "" + user_name)
+                            .appendQueryParameter("password", pass_word);
+
+                    String query = builder.build().getEncodedQuery();
+
+                    Log.d("test", query);
+
+                    OutputStream os = httpURLConnection.getOutputStream();
+
+                    BufferedWriter mBufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    mBufferedWriter.write(query);
+                    mBufferedWriter.flush();
+                    mBufferedWriter.close();
+                    os.close();
+
+                    httpURLConnection.connect();
+                    BufferedReader mBufferedInputStream = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    String inline;
+                    while ((inline = mBufferedInputStream.readLine()) != null) {
+                        Response += inline;
+                    }
+                    mBufferedInputStream.close();
+                    Log.d("response", Response);
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Response = "";
+                Log.d(Constants.TAG, "doInBackground() called with: " + "voids = [" + Response + "]");
+                return null;
+
+
+            }
+        }.execute();
+
+
     }
 }
--- Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/SplashScreen.java
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/SplashScreen.java
@@ -2,9 +2,9 @@ package in.indetech.cfg_samriddhi;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -22,11 +22,12 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 try {
                     Thread.sleep(1250);
-                    if(mPreferences.getBoolean(Constants.LOGIN_SHARED_PREFERENCE,false))
-                    {
-                        startActivity(new Intent(SplashScreen.this,Login.class));
-                    }else{
-                        startActivity(new Intent(SplashScreen.this,StudentDetails.class));
+                    if (mPreferences.getBoolean(Constants.LOGIN_SHARED_PREFERENCE, false)) {
+                        startActivity(new Intent(SplashScreen.this, StudentDetails.class));
+                        finish();
+                    } else {
+                        startActivity(new Intent(SplashScreen.this, Login.class));
+                        finish();
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
