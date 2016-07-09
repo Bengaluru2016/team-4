92a175138c8b727869cf8f7b2642aa0b7081e7ef
darshan <darshan.gowda008@gmail.com>
2016-07-09 21:50:38 +0530

    Student details two front end

--- Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetailsTwo.java
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetailsTwo.java
@@ -2,6 +2,7 @@ package in.indetech.cfg_samriddhi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentDetailsTwo extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class StudentDetailsTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details_two);
+
+
+
+    }
+
+    public void next(View view) {
+
+
+
     }
 }
