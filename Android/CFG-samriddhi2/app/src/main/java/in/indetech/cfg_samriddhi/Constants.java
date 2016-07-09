1c5253f3e52375a6711970b131d06cbe4f194ff5
darshan <darshan.gowda008@gmail.com>
2016-07-09 17:41:19 +0530

    Splash screen and shared preference to check login

---
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/Constants.java
@@ -0,0 +1,11 @@
+package in.indetech.cfg_samriddhi;
+
+/**
+ * Created by darshan on 09/07/16.
+ */
+public class Constants {
+
+    public static String PACKEAGE_ID = "in.indetech.cfg_samriddhi";
+    public static String LOGIN_SHARED_PREFERENCE = PACKEAGE_ID + "login";
+
+}
