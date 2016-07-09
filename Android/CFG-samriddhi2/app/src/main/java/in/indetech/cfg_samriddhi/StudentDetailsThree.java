e6a3e36588a2bd1c7abd6cea354e8bb1d07a7d8c
darshan <darshan.gowda008@gmail.com>
2016-07-09 22:42:53 +0530

    Student details three front end

---
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetailsThree.java
@@ -0,0 +1,20 @@
+package in.indetech.cfg_samriddhi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StudentDetailsThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_details_three);
+    }
+
+    public void submit(View view) {
+
+
+
+    }
+}
--- Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetailsTwo.java
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentDetailsTwo.java
@@ -1,23 +1,55 @@
 package in.indetech.cfg_samriddhi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class StudentDetailsTwo extends AppCompatActivity {
+
+
+    EditText fatherName, fatherOccupation, fatherIncome, fatherMobile;
+    EditText motherName, motherOccupation, motherIncome, motherMobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details_two);
 
+        init();
+
+    }
+
+    private void init() {
+
+        fatherName = (EditText) findViewById(R.id.fatherNameEditText);
+        fatherOccupation = (EditText) findViewById(R.id.fatherOccupationEditText);
+        fatherIncome = (EditText) findViewById(R.id.fatherIncomeEditText);
+        fatherMobile = (EditText) findViewById(R.id.fatherMobileNumber);
+
+        motherName = (EditText) findViewById(R.id.motherNameEditText);
+        motherOccupation = (EditText) findViewById(R.id.motherOccupationEditText);
+        motherIncome = (EditText) findViewById(R.id.motherIncomeEditText);
+        motherMobile = (EditText) findViewById(R.id.motherMobileNumber);
 
 
     }
 
     public void next(View view) {
 
+        StudentDetailsOne.studentdata.setFather_name(fatherName.getText().toString());
+        StudentDetailsOne.studentdata.setFather_occupation(fatherOccupation.getText().toString());
+        StudentDetailsOne.studentdata.setFather_income(fatherIncome.getText().toString());
+        StudentDetailsOne.studentdata.setFather_number(fatherMobile.getText().toString());
 
+        StudentDetailsOne.studentdata.setMother_name(motherName.getText().toString());
+        StudentDetailsOne.studentdata.setMother_occupation(motherOccupation.getText().toString());
+        StudentDetailsOne.studentdata.setMother_income(motherIncome.getText().toString());
+        StudentDetailsOne.studentdata.setMother_number(motherMobile.getText().toString());
+
+
+        startActivity(new Intent(StudentDetailsTwo.this,StudentDetailsThree.class));
 
     }
 }
