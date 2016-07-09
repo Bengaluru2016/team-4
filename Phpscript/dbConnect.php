548b76cae48ba762c98b288df7fae88673923cf4
susmithamarripalapu <susmitha.m20@gmail.com>
2016-07-09 18:32:26 +0530

    edited dbconnect
    test

--- Phpscript/dbConnect.php
+++ Phpscript/dbConnect.php
@@ -1,4 +1,8 @@
 <?php
+	define('HOST','http://ec2-54-169-33-156.ap-southeast-1.compute.amazonaws.com/');
+	define('USER','root');
+	define('PASS','darshan');
+	define('DB','Team4');
 	
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 ?>
