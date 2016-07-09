97a06e332082eff22d269281525288a77e516434
darshan <darshan.gowda008@gmail.com>
2016-07-09 20:59:31 +0530

    Student Data model added

---
+++ Android/CFG-samriddhi2/app/src/main/java/in/indetech/cfg_samriddhi/StudentData.java
@@ -0,0 +1,204 @@
+package in.indetech.cfg_samriddhi;
+
+/**
+ * Created by darshan on 09/07/16.
+ */
+public class StudentData {
+
+    String student_name, locality_name, surveyer_name;
+    String dob, age, gender, no_of_siblings, mother_tongue;
+    String education_level, previous_occupation, reason;
+
+    //father's details
+    String father_name, father_occupation, father_income, father_number;
+
+    //mother name
+    String mother_name, mother_occupation, mother_income, mother_number;
+
+    //native details
+    String state, district, address, native_contact;
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getLocality_name() {
+        return locality_name;
+    }
+
+    public void setLocality_name(String locality_name) {
+        this.locality_name = locality_name;
+    }
+
+    public String getSurveyer_name() {
+        return surveyer_name;
+    }
+
+    public void setSurveyer_name(String surveyer_name) {
+        this.surveyer_name = surveyer_name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNo_of_siblings() {
+        return no_of_siblings;
+    }
+
+    public void setNo_of_siblings(String no_of_siblings) {
+        this.no_of_siblings = no_of_siblings;
+    }
+
+    public String getMother_tongue() {
+        return mother_tongue;
+    }
+
+    public void setMother_tongue(String mother_tongue) {
+        this.mother_tongue = mother_tongue;
+    }
+
+    public String getEducation_level() {
+        return education_level;
+    }
+
+    public void setEducation_level(String education_level) {
+        this.education_level = education_level;
+    }
+
+    public String getPrevious_occupation() {
+        return previous_occupation;
+    }
+
+    public void setPrevious_occupation(String previous_occupation) {
+        this.previous_occupation = previous_occupation;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getFather_name() {
+        return father_name;
+    }
+
+    public void setFather_name(String father_name) {
+        this.father_name = father_name;
+    }
+
+    public String getFather_occupation() {
+        return father_occupation;
+    }
+
+    public void setFather_occupation(String father_occupation) {
+        this.father_occupation = father_occupation;
+    }
+
+    public String getFather_income() {
+        return father_income;
+    }
+
+    public void setFather_income(String father_income) {
+        this.father_income = father_income;
+    }
+
+    public String getFather_number() {
+        return father_number;
+    }
+
+    public void setFather_number(String father_number) {
+        this.father_number = father_number;
+    }
+
+    public String getMother_name() {
+        return mother_name;
+    }
+
+    public void setMother_name(String mother_name) {
+        this.mother_name = mother_name;
+    }
+
+    public String getMother_occupation() {
+        return mother_occupation;
+    }
+
+    public void setMother_occupation(String mother_occupation) {
+        this.mother_occupation = mother_occupation;
+    }
+
+    public String getMother_income() {
+        return mother_income;
+    }
+
+    public void setMother_income(String mother_income) {
+        this.mother_income = mother_income;
+    }
+
+    public String getMother_number() {
+        return mother_number;
+    }
+
+    public void setMother_number(String mother_number) {
+        this.mother_number = mother_number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNative_contact() {
+        return native_contact;
+    }
+
+    public void setNative_contact(String native_contact) {
+        this.native_contact = native_contact;
+    }
+}
